7db263a0f3d752e2c79f35485e18e141d3aba26c
Shivesh Dewangan <shivesh@example.com>
2026-07-28 12:31:50 +1000

    Make uvicorn Render-ready deployment settings

--- app.py
+++ app.py
@@ -278,6 +278,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8080,
-        reload=True
+        port=int(os.environ.get("PORT", 8080)),
+        reload=False
     )
